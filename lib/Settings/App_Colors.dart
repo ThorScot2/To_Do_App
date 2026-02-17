e0b28ef5395bbbb066b0264384e5f584db8efc02
Salem Ramadan <salemramdan12345@gmail.com>
2026-02-17 02:41:15 +0200

    Search Bar

---
+++ lib/Settings/App_Colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color appBarColor = Color(0xFF161616);
+
+  static const Color bodyColor = Color(0xFF161616);
+
+  static const Color fontColor = Color(0xFFffffff);
+
+  static const Color checkColor = Color(0xFFdfa20c);
+
+  static const Color floatingBtnColor = Color(0xFFe6a200);
+  static const Color checkedTaskColor = Color(0xFFe6a200);
+  static const Color cancelBtnColor = Color(0xFFe6a200);
+  static const Color saveBtnColor = Color(0xFFe6a200);
+  static const Color radioBtnColor = Color(0xFFe6a200);
+
+  static const Color toDoCardColor = Color(0xFF2f2f2f);
+
+  static const Color bottomSheetBacgroundColor = Color(0xFF1f1f1f);
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'Settings/App_Colors.dart';
+import 'features/tasks/view/HomePage.dart';
+import 'features/tasks/viewmodel/prov.dart';
+
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => TaskProvider(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.bodyColor,
+      ),
+      home: Homepage(),
+      routes: {
+        "home" : (context) =>  Homepage(),
+        // "accounts" : (context) => Accountspage(),
+
+      },
+    );
+  }
+}
